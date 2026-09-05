9a7d22b3d02bbe057d89d2477395b8a49c342607
Rishikant Namdev <rishikantnamdev8@gmail.com>
2026-09-05 15:48:01 +0530

    chore: reset base path to root for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Time-Manager/', // Ensures relative asset resolution on GitHub Pages
+  base: '/',
   plugins: [react()],
   esbuild: {
     legalComments: 'none',
